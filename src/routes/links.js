b5cc990d4b146e36528fd4feee3d815d289ebc7e
Byron2020 <vini.byron@gmail.com>
2020-08-05 02:06:06 -0500

    Tercer Commit

--- src/routes/links.js
+++ src/routes/links.js
@@ -3,11 +3,13 @@ const router= express.Router();
 
 const pool = require('../database');
 
-router.get('/add',(req, res)=>{
+const { isLoggedIn } = require('../lib/auth');
+
+router.get('/add', isLoggedIn, (req, res)=>{
     res.render('links/add');
 });
 
-router.post('/add',async(req, res)=>{
+router.post('/add', isLoggedIn, async(req, res)=>{
 const {correo_user,password_user,tipo_user,nombre_user,celular_user,direccion_user,ciudad_user,
     estrellas_user,estado_user}=req.body;
     const newuser={
@@ -19,7 +21,8 @@ const {correo_user,password_user,tipo_user,nombre_user,celular_user,direccion_us
         direccion_user,
         ciudad_user,
         estrellas_user,
-        estado_user        
+        estado_user,  
+       /*  id_user:req.user.id_user  */    
     };
     console.log(newuser);
     await pool.query('insert into users set ?',[newuser]);
@@ -28,13 +31,14 @@ const {correo_user,password_user,tipo_user,nombre_user,celular_user,direccion_us
     res.redirect('/links');  
 });
 
-router.get('/',async(req, res)=>{
+router.get('/',  isLoggedIn, async(req, res)=>{
+    /* 'slect * from users WHERE id_user = ?' , [req.user.id_user] */
     const list_users= await pool.query('SELECT * FROM users');
     res.render('links/list', {list_users});
 });
 
 
-router.get('/delete/:id_user', async(req, res)=> {
+router.get('/delete/:id_user', isLoggedIn, async(req, res)=> {
     const {id_user}= req.params;
     await pool.query('delete from users WHERE id_user= ?',[id_user,]);
     req.flash('success','Usuario eliminado correctamente');
@@ -42,14 +46,14 @@ router.get('/delete/:id_user', async(req, res)=> {
     res.redirect('/links');    
 });
 
-router.get('/edit/:id_user', async (req, res)=>{
+router.get('/edit/:id_user', isLoggedIn, async (req, res)=>{
     const {id_user}= req.params;
     const user= await pool.query('SELECT * FROM users WHERE id_user= ?',[id_user]);
     console.log('user[0]');
     res.render('links/edit',{user:user[0]});
 });
 
-router.post('/edit/:id_user',async(req,res)=>{
+router.post('/edit/:id_user', isLoggedIn, async(req,res)=>{
     const {id_user}= req.params;
     const {correo_user,password_user,tipo_user,nombre_user,celular_user,direccion_user,ciudad_user,
         estrellas_user,estado_user}=req.body;
